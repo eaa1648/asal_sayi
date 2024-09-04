4cc299f7c32c3afd02af122a01d82528addb5a56
eaa1648 <97223995+eaa1648@users.noreply.github.com>
2024-09-04 22:05:21 +0300

    asal sayi dogrulama uygulamasi

---
+++ PrimeCheckerSimple.cs
@@ -0,0 +1,14 @@
+public class PrimeCheckerSimple
+{
+    public bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        
+        for (int i = 2; i <= number / 2; i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
